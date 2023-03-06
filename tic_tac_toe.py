494a66a2c9cb63493169937a1c34fb4781331d2f
Arjun <iamarjunkharal@gmail.com>
2023-03-06 06:07:04 +0545

    logic builder

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,20 +8,20 @@ def board(value):
             {value[6]}  {value[7]}  {value[8]} \n''')
     print("this is sample of board where number reperesents position of 'X' and 'O'")
 
-value_for_board = "012345678"
+value_for_board = "123456789"
 board(value_for_board)
 
 #asking information with players
 def asking_personal_info():
     valid_marks = ['X','O']
-    print("in this section you need to give you personal info\n")
+    print("in this section you need to choose \n")
     player1_mark= input("which mark do you want to play, either O or X? \n").upper()
     try:
         if player1_mark == "X": 
             print("player 2 your mark is O\n")
             player2_mark = 'O' #string ma xan hai input haru
         else:
-            print("player 2 your mark is X")
+            print("\nplayer 2 your mark is X")
             player2_mark = 'X'
         if player1_mark not in valid_marks: 
             raise Exception
@@ -40,38 +40,33 @@ if player1_mark == "X" : choosing = lst1
 else: choosing = lst2
 
 #now gaming part
-count = 0 
-replacer = str()
-def gaming(choosing,value_for_board,count,replacer):
+def gaming(choosing,value_for_board):
     checker = []
+    count =  0 
+    #to convert string into list for assignment
+    lst_value_for_board = [i for i in value_for_board]
     while count < 9 : # for debug lets put value of count as 2
         if count % 2 == 0:
-            print(f"It is player one turn and he/she mark is {choosing[count]} \n")
-            player1_entry = int(input("enter number between 0 to 9 only\n"))`
+            print(f"It is player one turn and his/her mark is {choosing[count]} \n")
+            player1_entry = int(input("enter number between 0 to 8 only\n"))
+            lst_value_for_board[player1_entry] = choosing[count]
             checker.append(player1_entry)
-          #  print(choosing[count]) #debug
-            replacer = replacer + choosing[count]
-           # print(replacer) 
+          #  print(choosing[count]) #debug 
+           
         
         else:
-            print(f"It is player two turn and him/her mar is {choosing[count]} \n")
-            player2_entry = int(input('enter number between 0 to 9 only\n'))
+            print(f"It is player two turn and his/her mark is {choosing[count]} \n")
+            player2_entry = int(input('enter number between 0 to 8 only\n'))
+            lst_value_for_board[player2_entry] = choosing[count]
             checker.append(player2_entry)
-          #  print(choosing[count]) #debug
-            replacer = replacer + choosing[count]
+            #print(choosing[count]) #debug
 
         count += 1 
 
-    return replacer
-
-replacer = gaming(choosing, value_for_board,count,replacer)
-print(replacer) 
-
-#now to print final board 
-board(replacer)
-
-
-
+    return checker,lst_value_for_board
+#checker is to check whether the slot is already full or not
+checker,lst_value_for_board = gaming(choosing,value_for_board)
+board(lst_value_for_board)
 
 
 
